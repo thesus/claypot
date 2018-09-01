e83fd7f318d519b7c88f21b91c30ac8152386332
David Sieger <dms@iterra-gmbh.de>
2018-09-01 11:01:48 +0200

    Simple admin interface

---
+++ claypot/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from . import models
+
+
+admin.site.register(models.Ingredient)
+admin.site.register(models.Recipe)
+admin.site.register(models.RecipeIngredient)
+admin.site.register(models.Unit)
