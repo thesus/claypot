fec8c919d0f5cefbab2ff0cf8066efaa319cc5bb
Till Wesselmann <till@cryptec.at>
2018-09-02 12:47:03 +0200

    Vue Multiselect and convenient ingredient selector

--- claypot/contrib/static/js/recipe.js
+++ claypot/contrib/static/js/recipe.js
@@ -1,44 +1,74 @@
 import Vue from 'vue';
 import axios from 'axios';
 import 'babel-polyfill';
+import Multiselect from 'vue-multiselect';
+
 
 new Vue({
-    el: '#recipe',
-    delimiters: ['{(', ')}'],
-    data () {
-        return {
-            'recipe': {}
+  el: '#recipe',
+  delimiters: ['{(', ')}'],
+  components: {
+    'multiselect': Multiselect
+  },
+  data () {
+    return {
+      'searches': [],
+      'newIngredients': [],
+      'recipe': {},
+      'ingredients': [],
+      'url': '/' + location.pathname.substring(1),
+      'config': {
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest'
         }
-    },
-    mounted () {
-        this.getRecipe()
-    },
-    methods: {
-        async getRecipe () {
-            // let url = '/' + location.pathname.substring(1)
-            // var response = axios.get(url, {
-            //     headers: {
-            //         'X-Requested-With': 'XMLHttpRequest'
-            //     }
-            // }).catch((error) => {
-            //     console.log(error)
-            // })
-            // this.$set(this, 'recipe', await response.data)
-            this.$set(
-                this,
-                'recipe', {
-                    title: '',
-                    instructions: '',
-                    ingredients: [
-                        {
-                            extra: 'apfel'
-                        },
-                        {
-                            extra: 'banane'
-                        }
-                    ]
-                }
-            )
-        }
+      }
     }
+  },
+  mounted () {
+    this.getRecipe()
+    this.findIngredients('', -1)
+  },
+  computed: {
+    getIngredients () {
+      return this.ingredients.concat(this.newIngredients)
+    }
+  },
+  methods: {
+    isSearchLoading(id) {
+      return this.searches.includes(id)
+    },
+    async getRecipe () {
+      var response = await axios.get(this.url, this.config).catch(
+        (error) => {
+          console.log(error)
+        }
+      )
+
+      let recipe = response.data
+      this.$set(this, 'recipe', response.data)
+    },
+    async findIngredients(search, id) {
+      this.searches.push(id)
+      let searchUrl = '/ingredients?search=' + search
+      var response = await axios.get(searchUrl, this.config).catch(
+        (error) => {
+          console.log(error)
+          return
+        }
+      )
+      this.$set(this, 'ingredients', response.data.search)
+      let index = this.searches.indexOf(id)
+      if (index > -1) {
+        this.searches.splice(index, 1)
+      }
+    },
+    createIngredient(search, id) {
+      this.$set(
+        this.recipe.recipe_ingredients[id],
+        'ingredient',
+        search
+      )
+      this.newIngredients.push(search)
+    }
+  }
 })
