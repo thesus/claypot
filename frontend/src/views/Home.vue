dbcbb8bf2a1d05879868f193afdeaf839c075dda
Till Wesselmann <till@cryptec.at>
2018-09-15 23:54:05 +0200

    Apollo :(

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,49 @@
 <template>
   <div>
-    helo
+      {{ recipe }}
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'home'
+  name: 'home',
+  apollo: {
+    recipe: gql`query {
+          recipes(instructions_Icontains: "180") {
+            edges {
+              node {
+                id
+                title
+                instructions
+                recipeIngredients {
+                  edges {
+                    node {
+                      id
+                      ingredient {
+                        name
+                      }
+                      amountType
+                      amountNumeric
+                      amountApprox
+                      unit {
+                        id
+                        name
+                        code
+                        namePlural
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }`
+  },
+  data () {
+    return {
+      recipe: null
+    }
+  }
 }
 </script>
