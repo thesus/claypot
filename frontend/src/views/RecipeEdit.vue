40cee8d11ea5bdadc5190937bb21ac85ed6cd472
David Sieger <dms@iterra-gmbh.de>
2019-01-19 00:25:50 +0100

    Restored edit view. Cannot save, yet.

--- frontend/src/views/RecipeEdit.vue
+++ frontend/src/views/RecipeEdit.vue
@@ -1,9 +1,9 @@
 <template>
   <div v-if="loading">Loading...</div>
   <div v-else-if="error">Error loading data</div>
-  <article v-else-if="data && data.recipe">
+  <article v-else-if="recipe">
     <header>
-      <input v-model="title">
+      <input v-model="recipe.title">
     </header>
 
     <table>
@@ -14,15 +14,15 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="ingredient in recipeIngredients" :key="ingredient.name">
-          <td><input v-model="ingredient.amountNumeric">&nbsp;<input v-model="ingredient.code"></td>
-          <td><input v-model="ingredient.name"></td>
+        <tr v-for="ingredient in recipe.recipe_ingredients" :key="ingredient.ingredient.name">
+          <td><input v-model="ingredient.amount_numeric">&nbsp;<input v-model="ingredient.unit.code"></td>
+          <td><input v-model="ingredient.ingredient.name"></td>
         </tr>
       </tbody>
     </table>
     <button @click.prevent="addIngredient">Add</button>
 
-    <p><textarea v-model="instructions"></textarea></p>
+    <p><textarea v-model="recipe.instructions"></textarea></p>
 
     <footer>
       <p>posted by {{ user }}</p>
@@ -34,13 +34,18 @@
 </template>
 
 <script>
+import {api, endpoints} from '@/api'
+
 export default {
   data () {
     return {
-      title: '',
-      recipeIngredients: [],
-      instructions: ''
+      loading: false,
+      error: false,
+      recipe: {}
     }
+  },
+  mounted () {
+    this.update()
   },
   computed: {
     recipeId () {
@@ -53,6 +58,24 @@ export default {
   methods: {
     addIngredient () {
       this.recipeIngredients.push({name: '', amountNumeric: 0, code: ''})
+    },
+    async update () {
+      if (!this.recipeId) {
+        return
+      }
+      try {
+        this.loading = true
+        const r = await api(endpoints.fetch_recipe(this.recipeId))
+        this.loading = false
+        if (r.ok) {
+          this.recipe = await r.json()
+        } else {
+          throw new Error("Display some kind of error")
+        }
+      } catch (err) {
+        // TODO: Display some kind of error
+        this.error = true
+      }
     },
     save () {
       console.log('save')
