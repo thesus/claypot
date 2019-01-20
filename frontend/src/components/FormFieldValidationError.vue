b4f725fc388a3e0dfc5e06c2edd2a2cabcf86bba
David Sieger <dms@iterra-gmbh.de>
2019-01-20 12:02:54 +0100

    Form validation for recipe edit form

---
+++ frontend/src/components/FormFieldValidationError.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="errors">
+    <p v-if="errors.length === 1">{{ errors[0] }}</p>
+    <ul v-else>
+      <li v-for="(err, i) in errors" :v-key="i">
+        <p>{{ err }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'form-field-validation-error',
+  props: {
+    errors: Array
+  }
+}
+</script>
--- frontend/src/views/RecipeEdit.vue
+++ frontend/src/views/RecipeEdit.vue
@@ -3,40 +3,62 @@
   <div v-else-if="error">{{ $t('recipe_edit.loading_error') }}</div>
   <article v-else-if="recipe">
     <header>
-      <input v-model="recipe.title" :disabled="saving">
+      <div><input v-model="recipe.title" :disabled="saving" :class="{'form-error': !!errors.title.length}"></div>
+      <form-field-validation-error :errors="errors.title" />
     </header>
 
     <table>
       <thead>
         <tr>
           <th>{{ $t('recipe_edit.amount') }}</th>
+          <th>{{ $t('recipe_edit.unit') }}</th>
           <th>{{ $t('recipe_edit.ingredient') }}</th>
+          <th>{{ $t('recipe_edit.ingredient_extra') }}</th>
           <th></th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(ingredient, i) in recipe.recipe_ingredients" :key="i">
-          <td><input v-model="ingredient.amount_numeric">&nbsp;<input v-model="ingredient.unit"></td>
-          <td><input v-model="ingredient.ingredient" :disabled="saving"></td>
+          <td>
+            <div><input v-model="ingredient.amount_numeric" :class="{'form-error': !!recipeIngredientError(i).amount_numeric.length}"></div>
+            <form-field-validation-error :errors="recipeIngredientError(i).amount_numeric" />
+          </td>
+          <td>
+            <div><input v-model="ingredient.unit" :disabled="saving" :class="{'form-error': !!recipeIngredientError(i).unit.length}"></div>
+            <form-field-validation-error :errors="recipeIngredientError(i).unit" />
+          </td>
+          <td>
+            <div><input v-model="ingredient.ingredient" :disabled="saving" :class="{'form-error': !!recipeIngredientError(i).ingredient.length}"></div>
+            <form-field-validation-error :errors="recipeIngredientError(i).ingredient" />
+          </td>
+          <td>
+            <input v-model="ingredient.ingredient_extra" :disabled="saving" :class="{'form-error': !!recipeIngredientError(i).ingredient_extra.length}">
+            <form-field-validation-error :errors="recipeIngredientError(i).ingredient_extra" />
+          </td>
           <td><button :disabled="saving" @click="recipe.recipe_ingredients.splice(i)">{{ $t('recipe_edit.remove') }}</button></td>
         </tr>
       </tbody>
     </table>
     <button @click.prevent="addIngredient" :disabled="saving">{{ $t('recipe_edit.add') }}</button>
 
-    <p><textarea v-model="recipe.instructions" :disabled="saving"></textarea></p>
+    <div>
+      <div><textarea v-model="recipe.instructions" :disabled="saving"></textarea></div>
+      <form-field-validation-error :errors="errors.instructions" />
+    </div>
 
     <footer>
       <p>{{ $t('recipe_edit.posted_by',  {user: user}) }}</p>
     </footer>
 
-    <button @click.prevent="save" :disabled="saving">{{ $t('recipe_edit.save') }}</button>
+    <div><button @click.prevent="save" :disabled="saving">{{ $t('recipe_edit.save') }}</button></div>
+    <div v-if="errors.client_side">{{ errors.client_side }}</div>
   </article>
   <div v-else>{{ $t('recipe_edit.no_data') }}</div>
 </template>
 
 <script>
 import {api, endpoints} from '@/api'
+import FormFieldValidationError from '@/components/FormFieldValidationError'
 
 const amount_types = {
   none: 1,
@@ -45,12 +67,21 @@ const amount_types = {
 }
 
 export default {
+  components: {
+    FormFieldValidationError,
+  },
   data () {
     return {
       loading: false,
       error: false,
+      errors: {
+        title: [],
+        recipe_ingredients: [],
+        instructions: [],
+        client_side: '',
+      },
       saving: false,
-      recipe: {}
+      recipe: {},
     }
   },
   mounted () {
@@ -62,11 +93,21 @@ export default {
     },
     user () {
       return this.$t('recipe_edit.unknown_user')
+    },
+    recipeIngredientError () {
+      return i => {
+        const e = this.errors.recipe_ingredients
+        const r = e.length > i ? e[i] : {}
+        for (let p of ['ingredient', 'ingredient_extra', 'amount_numeric', 'unit']) {
+          r[p] = r[p] || []
+        }
+        return r
+      }
     }
   },
   methods: {
     addIngredient () {
-      this.recipe.recipe_ingredients.push({ingredient: '', amount_numeric: 0, unit: ''})
+      this.recipe.recipe_ingredients.push({ingredient: '', ingredient_extra: '', amount_numeric: 0, unit: ''})
     },
     async update () {
       if (!this.recipeId) {
@@ -107,6 +148,7 @@ export default {
       try {
         const r = await api(endpoints.post_recipe(this.recipeId), d, {method:'put'})
         if (r.ok) {
+          this.errors.client_side = ''
           this.recipe = await r.json()
           // TODO: Notify user about success.
           this.$router.push({
@@ -117,12 +159,24 @@ export default {
           })
         } else {
           // TODO: Notify user about broken fields?
+          const errors = await r.json()
+          this.errors.title = errors.title || []
+          this.errors.recipe_ingredients = errors.recipe_ingredients || []
+          this.errors.instructions = errors.instructions || []
         }
       } catch (err) {
-        // TODO: Notify user about network error.
+        this.errors.client_side = err.message
       }
       this.saving = false
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/modules/inputs.scss';
+
+table {
+  width: 100%;
+}
+</style>
