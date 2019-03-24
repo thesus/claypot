f6a9834e514e6ba6d8f769ac2baf7626c2b3d75d
Till Wesselmann <till@cryptec.at>
2019-03-24 18:51:01 +0100

    Fit images with real delete button and remove debug

--- frontend/src/components/ImageUpload.vue
+++ frontend/src/components/ImageUpload.vue
@@ -18,8 +18,8 @@
         v-for="(image, index) in images"
         :key="index"
         class="thumbnail"
-        @click="removeImage(index)"
       >
+        <span class="delete" @click="removeImage(index)">Delete</span>
         <img
           :src="image.url"
           :class="{
@@ -74,7 +74,6 @@ export default {
       this.$emit('input', this.images.filter(image => image.success === true).map(image => image.id))
     },
     removeImage (id) {
-      console.log(id)
       this.$delete(this.images, id)
 
       this.update()
@@ -158,12 +157,38 @@ input[type=file] {
 .thumbnail {
   width: $thumbnail-size;
   height: $thumbnail-size;
+  position: relative;
+  display: block;
+
+  .delete {
+    display: none;
+    background-color: white;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    right: 2px;
+    z-index: 1000;
+    cursor: pointer;
+  }
+
+  &:hover {
+    .delete {
+      display: block;
+    }
+  }
 }
+
 
 img {
   height: 100%;
   width: 100%;
   object-fit: cover;
+  box-sizing: border-box;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 
   &.loading {
     filter: blur(1px);
