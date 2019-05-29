d7d54a97e5faf6cca59ac23bb5b8f22a439ec981
Till Wesselmann <till@cryptec.at>
2019-05-29 13:18:01 +0200

    Add debounced search input to frontend

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <article>
-    <h1>{{ $t('home.all_recipes') }}</h1>
-    <router-link
-      v-if="isLoggedIn"
-      :to="{name: 'recipe-add'}"
-    >
-      {{ $t('home.add') }}
-    </router-link>
-    <RecipeList :filters="filters" />
+    <div class="header">
+      <DebounceInput v-model="search" :placeholder="$t('home.search')" class="search"/>
+      <router-link
+        v-if="isLoggedIn"
+        :to="{name: 'recipe-add'}"
+        class="link"
+      >
+        {{ $t('home.add') }}
+      </router-link>
+    </div>
+    <RecipeList :filters="getFilters" />
   </article>
 </template>
 
@@ -16,6 +19,7 @@ import Vue from 'vue'
 import { mapGetters } from 'vuex'
 
 import RecipeList from '@/components/RecipeList'
+import DebounceInput from '@/components/DebounceInput'
 
 import { api, endpoints } from '@/api'
 
@@ -23,14 +27,17 @@ export default {
   name: 'Home',
   components: {
     RecipeList,
+    DebounceInput
   },
-  props: {
-    filters: {
-      type: Object,
-      default: () => ({}),
-    },
+  data () {
+    return {
+      search: ''
+    }
   },
   computed: {
+    getFilters () {
+      return this.search === '' ? {} : {'search': this.search}
+    },
     ...mapGetters([
       'isLoggedIn',
     ]),
@@ -44,3 +51,21 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.header {
+  margin-bottom: 10px;
+  display: flex;
+
+  .link {
+    width: 120px;
+  }
+
+  @media screen and (max-width: 500px) {
+    flex-direction: column-reverse;
+    .link {
+      width: initial;
+    }
+  }
+}
+</style>
