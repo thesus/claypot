7a84ef6a4e713356096f46c8f5d8bce488be9976
Till Wesselmann <till@cryptec.at>
2019-07-21 17:27:17 +0200

    Add initial version of extended scale input

--- frontend/src/components/ScaleInput.vue
+++ frontend/src/components/ScaleInput.vue
@@ -4,19 +4,103 @@
       v-if="modal"
       :title="$t('scaling.title')"
       @close="modal = false"
+      class="modal"
     >
 
+      <sup>
+        <button
+          class="btn"
+          @click="increaseNumerator"
+          :disabled="denominator < 2"
+        >
+          +
+        </button>
+      </sup>
+      &frasl;
+      <sub>
+        <button
+          class="btn"
+          @click="changeDenominator(+1)"
+        >
+          +
+        </button>
+      </sub>
+      <button
+        class="btn full"
+        @click="increaseFull"
+      >
+        +
+      </button>
+
+
+      <div class="extended">
+        <span
+          v-if="!isInteger"
+          class="fraction"
+          :class="{ 'ignore': displayNumerator === 0 }"
+        >
+          <sup>{{ displayNumerator }}</sup>
+          &frasl;
+          <sub>{{ denominator }}</sub>
+        </span>
+        <span
+          v-if="n > 0"
+          class="full"
+        >
+          {{ n }}
+        </span>
+      </div>
+
+      <sup>
+        <button
+          class="btn"
+          :disabled="denominator < 2 || (n === 0 && numerator < 2)"
+          @click="decreaseNumerator"
+        >
+          -
+        </button>
+      </sup>
+      &frasl;
+      <sub>
+        <button
+          class="btn"
+          :disabled="denominator < 2"
+          @click="changeDenominator(-1)"
+        >
+          -
+        </button>
+      </sub>
+      <button
+        class="btn full"
+        :disabled="n === 0"
+        @click="decreaseFull"
+      >
+        -
+      </button>
     </Modal>
 
 
-    <div class="button" @click="decrease">-</div>
-    <span @click="modal = true" v-if="denominatorHidden">{{ numerator }}</span>
-    <span @click="modal = true" class="fraction" v-else>
-      <sup>{{ numerator }}</sup>&frasl;<sub>{{ denominator }}</sub>
-    </span>
+    <div class="button" @click="decreaseSimple">-</div>
+    <div class="extended" @click="modal = true">
+      <span
+        v-if="!isInteger"
+        class="fraction"
+        :class="{ 'ignore': displayNumerator === 0 }"
+      >
+        <sup>{{ displayNumerator }}</sup>
+        &frasl;
+        <sub>{{ denominator }}</sub>
+      </span>
+      <span
+        v-if="n > 0"
+        class="full"
+      >
+        {{ n }}
+      </span>
+    </div>
     <div
       class="button"
-      @click="increase"
+      @click="increaseSimple"
     >
       +
     </div>
@@ -49,6 +133,19 @@ export default {
         return 1.0
       }
     },
+    displayNumerator () {
+      if (this.numerator / this.denominator >= 1) {
+        return this.numerator % this.denominator
+      } else {
+        return this.numerator
+      }
+    },
+    n () {
+      return Math.floor(this.numerator / this.denominator)
+    },
+    isInteger () {
+      return this.denominator === 1
+    }
   },
   watch: {
     fraction () {
@@ -56,7 +153,30 @@ export default {
     }
   },
   methods: {
-    increase () {
+    increaseNumerator () {
+      this.numerator++
+    },
+    decreaseNumerator () {
+      if (this.numerator >= 1) {
+        this.numerator--
+      }
+    },
+    changeDenominator (v) {
+      if (this.denominator + v >= 1) {
+        let c = Math.floor(this.numerator / this.denominator)
+        let n = this.numerator % this.denominator
+
+        this.denominator += v
+        this.numerator = this.denominator * c + n
+      }
+    },
+    increaseFull () {
+      this.numerator += this.denominator
+    },
+    decreaseFull () {
+      this.numerator -= this.denominator
+    },
+    increaseSimple () {
       if (!isNaN(Number(this.numerator)) || !isNaN(Number(this.denominator))) {
         if (this.denominatorHidden) {
           this.numerator = Number(this.numerator) + 1
@@ -70,7 +190,7 @@ export default {
         this.numerator = 1
       }
     },
-    decrease () {
+    decreaseSimple () {
       if (isNaN(Number(this.numerator)) || isNaN(Number(this.denominator))) {
         this.numerator = 1
         this.denominator = 1
@@ -89,6 +209,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/modules/inputs.scss';
 @import '@/modules/variables.scss';
 
 .scale {
@@ -114,7 +235,8 @@ export default {
   }
 }
 
-span {
+
+.simple {
   display: inline-block;
   padding: 0 2px 0 2px;
   width: 18px;
@@ -129,6 +251,28 @@ span {
     @media screen and (max-width: 500px) {
       line-height: 22px;
       font-size: 14px;
+    }
+  }
+}
+
+.extended {
+  .full {
+    margin-left: 4px;
+  }
+
+  .fraction {
+    &.ignore {
+      color: gray;
+    }
+  }
+}
+
+.modal {
+  .btn {
+    height: initial;
+
+    &.full {
+      margin-left: 4px;
     }
   }
 }
