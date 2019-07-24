9c429de5eb61591e59d9aa898711b31a1fb24c4a
David Sieger <dms@iterra-gmbh.de>
2019-07-24 22:57:48 +0200

    Try fix for scale input

--- frontend/src/components/ScaleInput2.vue
+++ frontend/src/components/ScaleInput2.vue
@@ -1,22 +1,56 @@
 <template>
   <span>
-    <span @click="showModal = true">
-      <span>{{ full }}</span>
-      <span v-if="remainder !== 0">
-        <sup>{{ remainder }}</sup>
-        /
-        <sub>{{ denominator }}</sub>
+    <span>
+      <button class="btn" @click="quickDecrease">-</button>
+      <span @click="showModal = true">
+        <span v-if="full > 0">{{ full }}</span>
+        <span v-if="remainder !== 0">
+          <sup>{{ remainder }}</sup>
+          /
+          <sub>{{ denominator }}</sub>
+        </span>
       </span>
+      <button class="btn" @click="quickIncrease">+</button>
     </span>
     <Modal v-if="showModal" @close="showModal = false">
       <div class="fraction-modal">
-        <div>
-          <input class="full" :value="full">
+        <div class="number-box">
+          <div>
+            <button class="btn" @click="increaseMultiplyer">+</button>
+          </div>
+          <div>
+            <input class="input full" min="1" :value="multiplyer">
+          </div>
+          <div>
+            <button class="btn" @click="decreaseMultiplyer">-</button>
+          </div>
         </div>
+        <div class="calc-hint">×</div>
         <div class="fraction">
-          <input class="remainder" :value="remainder">
-          <input class="denominator" :value="denominator">
+          <div class="number-box">
+            <div>
+              <button class="btn" @click="decreaseNumerator">-</button>
+            </div>
+            <div>
+              <input class="input" min="1" :value="numerator">
+            </div>
+            <div>
+              <button class="btn" @click="increaseNumerator">+</button>
+            </div>
+          </div>
+          <div class="number-box">
+            <div>
+              <button class="btn" @click="decreaseDenominator">-</button>
+            </div>
+            <div>
+              <input class="input denominator" min="1" :value="denominator">
+            </div>
+            <div>
+              <button class="btn" @click="increaseDenominator">+</button>
+            </div>
+          </div>
         </div>
+        <div class="calc-hint">={{ resultStr }}</div>
       </div>
     </Modal>
   </span>
@@ -37,6 +71,7 @@ export default {
   },
   data () {
     return {
+      multiplyer: 1.0,
       numerator: 1.0,
       denominator: 1.0,
       showModal: false,
@@ -44,27 +79,125 @@ export default {
   },
   computed: {
     full () {
-      return Math.floor(this.numerator / this.denominator)
+      return Math.floor(this.multiplyer * this.numerator / this.denominator)
+    },
+    result () {
+      return this.multiplyer * (this.numerator / this.denominator)
+    },
+    resultStr () {
+      return new Intl.NumberFormat().format(this.result)
     },
     remainder () {
-      return this.numerator % this.denominator
+      return (this.multiplyer * this.numerator) % this.denominator
+    },
+  },
+  watch: {
+    result () {
+      this.$emit('input', this.result)
+    },
+  },
+  methods: {
+    increaseNumerator () {
+      this.numerator += 1
+    },
+    decreaseNumerator () {
+      this.numerator = Math.max(this.numerator - 1, 1)
+    },
+    increaseDenominator () {
+      this.denominator += 1
+    },
+    decreaseDenominator () {
+      this.denominator = Math.max(this.denominator - 1, 1)
+    },
+    increaseMultiplyer () {
+      this.multiplyer += 1
+    },
+    decreaseMultiplyer () {
+      this.multiplyer = Math.max(this.multiplyer - 1, 1)
+    },
+    quickIncrease () {
+      if (this.multiplyer === 1) {
+        if (this.denominator <= 1) {
+          this.numerator += 1
+        } else {
+          this.denominator -= 1
+        }
+      } else {
+        this.multiplyer += 1
+      }
+    },
+    quickDecrease () {
+      if (this.multiplyer === 1) {
+        if (this.numerator > 1) {
+          this.numerator -= 1
+        } else {
+          this.denominator += 1
+        }
+      } else {
+        this.multiplyer = Math.max(this.multiplyer - 1, 1)
+      }
     },
   },
 }
 </script>
 
 <style lang="scss" scoped>
+@import '@/modules/inputs.scss';
+
+input {
+  /*border: 0px;*/
+  text-align: center;
+}
+
 .fraction-modal {
   display: flex;
   flex-direction: row;
 
-  & > div {
-    flex: 1;
+  & > .number-box {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .calc-hint {
+    font-size: 400%;
+    margin: auto;
+    white-space: nowrap;
+  }
+
+  .full {
+    height: 100%;
+    font-size: 600%;
+    border: 0px;
   }
 
   .fraction {
+    margin: auto;
     display: flex;
     flex-direction: column;
+    position: relative;
+
+    & > .number-box {
+      display: flex;
+      flex-direction: row;
+
+      & > div {
+        flex: 1;
+
+        &:nth-child(odd) {
+          /* vertical align of buttons */
+          margin: auto;
+        }
+      }
+    }
+
+    input {
+      height: 100%;
+      font-size: 300%;
+      border-width: 0px;
+    }
+    & > div:first-child {
+      border-bottom: 4px solid black;
+    }
   }
 }
 </style>
