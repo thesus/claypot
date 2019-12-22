4d216eefa539e65af803aaff1b92958cedfa6d28
Till Wesselmann <till@cryptec.at>
2019-12-22 12:07:20 +0100

    Fix access on undefined variable

--- frontend/src/components/recipe-edit/RecipeEditForm.vue
+++ frontend/src/components/recipe-edit/RecipeEditForm.vue
@@ -463,13 +463,13 @@ export default {
           { data: this.recipe_dirty, recipe: this.recipe.id },
           { method: this.draft ? 'put' : 'post' }
         )
+
+        /* If a new draft is created, set the id for the next save */
+        if (!this.draft && r.ok) {
+          this.draft = (await r.json()).id
+        }
       } catch (e) {
         console.log(e)
-      }
-
-      /* If a new draft is created, set the id for the next save */
-      if (!this.draft && r.ok) {
-        this.draft = (await r.json()).id
       }
     },
     async deleteDraft (id) {
