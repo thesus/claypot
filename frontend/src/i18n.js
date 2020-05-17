1c606c96997457aa9a1089a7923b24d5db11f078
David Sieger <dms@iterra-gmbh.de>
2020-05-17 12:42:47 +0200

    Automatically pick users preferred language
    Also remove some cargo-cult from vue.config.js

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -16,8 +16,43 @@ function loadLocaleMessages () {
   return messages
 }
 
+let locale = 'en'  // initialized to fallback value
+const messages = loadLocaleMessages()
+
+/* Note: This has a weakness, that is not relevant now, but might be at
+ * some point. We will not properly handle specific available locales.
+ * E. g. we will not pass out an available "en-gb", when a user requests
+ * a more generic "en", but we have no generic "en" available.
+ */
+const availableLanguages = new Set(Object.keys(messages).map(i => i.toLowerCase()))
+
+/* find the preferred language of the user. We do so by looking through
+ * the languages the user likes and then prefering any verison of that
+ * language we have over other languages the user likes. Better matches
+ * are prefered to more loose matches, e. g. an available "en-gb" locale
+ * would be prefered over a generic "en" locale.
+ *
+ * Note that this means if a user informas us, they would like the languages
+ * ["en-us", "de", "en"], we will look for "en-us" first, then *any* "en" locale,
+ * then for "de" and then for "en".
+ *
+ * We only support "xx-xx" and "xx" format, not the whole BCP47.
+ */
+findLangLoop:
+  for (const preferredLanguage of navigator.languages) {
+    const langParts = preferredLanguage.toLowerCase().split('-')
+    // looking for increasingly more generic version of language
+    for (let i = langParts.length; i >= 1; i--) {
+      const lookFor = langParts.slice(0, i).join('-')
+      if (availableLanguages.has(lookFor)) {
+        locale = lookFor
+        break findLangLoop
+      }
+    }
+  }
+
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
+  locale: locale,
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-  messages: loadLocaleMessages()
+  messages
 })
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,24 +1,26 @@
 module.exports = {
   chainWebpack: config => {
-     config.resolve
-       .extensions
-         .prepend('.mjs')
+    config.resolve
+      .extensions
+        .prepend('.mjs')
 
-     config.module
-       .rule('mjs')
-         .test(/\.mjs$/)
-         .include
-           .add(/node_modules/)
-           .end()
-         .type('javascript/auto')
-   },
-
+    config.module
+      .rule('mjs')
+        .test(/\.mjs$/)
+        .include
+          .add(/node_modules/)
+          .end()
+        .type('javascript/auto')
+  },
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
+      // there used to be more configuration for vue-i18n here, but it is handled in src/i18n.js now.
+
+      // this option is not actually from vue-i18n, but from vue-cli-plugin-i18n. They
+      // are not really saying, what it is for. I don't see a difference, when changing the value.
+      // But since we have to keep the `i18n` key in pluginOptions anyways, because otherwise
+      // vue-cli-plugin-i18n breaks the build process, I'll just cargo-cult this.
+      enableInSFC: true
     }
   }
 }
